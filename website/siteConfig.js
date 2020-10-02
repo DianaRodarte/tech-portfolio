4b6d9142ce5234a0d54618a840038f24f86ce042
Diana Rodarte <44706654+DianaRodarte@users.noreply.github.com>
2020-10-02 14:25:02 -0500

    I started  my technical portfolio!

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Diana Rodarte', // Title for your website.
+    tagline: 'Innovative Computer Science graduate. Passionate interest in Machine Learning and Data Science.',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://DianaRodarte.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
+        linkedin: 'dianarodarte',
         twitter: '',
-        github: '',
+        github: 'DianaRodarte',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
